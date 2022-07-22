1f8942e797bf7b0a0166ae984e5786b282e45289
ilya.rubinsky <ilya.rubinsky@gmail.com>
2022-07-22 14:09:53 +0300

    Initial commit

---
+++ src/main/java/intellistart/task/Intellistarttask/IntellistartTaskApplication.java
@@ -0,0 +1,13 @@
+package intellistart.task.Intellistarttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellistartTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellistartTaskApplication.class, args);
+	}
+
+}
